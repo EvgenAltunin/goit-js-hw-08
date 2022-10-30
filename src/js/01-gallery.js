b943ed8e7fcb91caedb282097690f1dea74a4bb1
Yevhen2701 <106544836+EvgenAltunin@users.noreply.github.com>
2022-10-30 22:47:45 +0200

    1 task was completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 
