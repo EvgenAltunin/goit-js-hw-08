8ccd934de80f73c8ba3b42d6dbf8a68873a36249
Yevhen2701 <106544836+EvgenAltunin@users.noreply.github.com>
2022-10-30 22:45:23 +0200

    Code for 1 task was copied from 7 homework

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryItemsMurkup = createGalleryItemsMurkup(galleryItems);
+    
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMurkup);
+
+function createGalleryItemsMurkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="" title="${description}"/>
+            </a>
+        </div>
+        `
+    })
+        .join('');
+    
+}
+
+let gallery = new SimpleLightbox('.gallery a', {captionDelay: 250});
+gallery.on('show.simplelightbox', function () {
+    
+});
